54f1a3263a958d9b5baf23f1b7917336e0926e49
joncloud <jdberube@gmail.com>
2018-03-31 11:48:13 -0700

    Adds outdir tests

--- tests/Sage.UnitTests/OutDirTests.cs
+++ tests/Sage.UnitTests/OutDirTests.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace Sage.UnitTests
 {
     public class OutDirTests
     {
+        [Sql]
+        public void ShouldPersistFilesGivenOutDir()
+        {
+            using (var testDirectory = new TestDirectory())
+            {
+                var _ = TestHarness.Run(
+                    () => Program.AsJson(SqlAttribute.ConnectionString, testDirectory.AbsolutePath),
+                    new[]
+                    {
+                        new Query { Name = "Query1", CommandText = "SELECT 1 [Num]" },
+                        new Query { Name = "Query2", CommandText = "SELECT 2 [Num]" },
+                        new Query { Name = "Query3", CommandText = "SELECT 3 [Num]" },
+                        new Query { Name = "Query4", CommandText = "SELECT 4 [Num]" }
+                    }
+                );
+
+                var actual = testDirectory.GetFiles().Select(x => x.Name);
+                var expected = new[] { "Query1", "Query2", "Query3", "Query4" };
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        class TestDirectory : IDisposable
+        {
+            readonly DirectoryInfo _directory;
+            public string AbsolutePath => _directory.FullName;
+
+            public TestDirectory()
+            {
+                string path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                _directory = Directory.CreateDirectory(path);
+            }
+
+            public IEnumerable<FileInfo> GetFiles() =>
+                _directory.GetFiles();
+
+            public void Dispose()
+            {
+                _directory.Delete(recursive: true);
+            }
+        }
     }
 }
