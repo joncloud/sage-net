b36ada3871fc286c7c85f837d556aaf240c2f43c
joncloud <jdberube@gmail.com>
2018-03-31 16:43:34 -0700

    Adds better documentation

--- tests/Sage.UnitTests/Properties/AssemblyInfo.cs
+++ tests/Sage.UnitTests/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 ﻿using Xunit;
 
+// Run all tests in serial to ensure that swapping of console 
+// streams do not have unintended impacts against other tests.
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
